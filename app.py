a2902b9c595b009afe37a09552d1fb83ba23704d
kacperjablonski <k.jablonski92@outlook.com>
2022-02-01 15:02:30 +0100

    change function and better code :P

--- app.py
+++ app.py
@@ -3,49 +3,46 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from flask import Flask, render_template
+from collections import ChainMap
 
+NBP_API_EX_RATES_URL = 'http://api.nbp.pl/api/exchangerates/tables/A/'
+CINKCIARZ_SCRAPING_RATE_URL = 'https://cinkciarz.pl/wa/pe/transactional?subscriptionId=PLN&unit=50000'
 
 app = Flask(__name__)
 
 
-NBP_API_GOLD_PRICE_URL = 'http://api.nbp.pl/api/cenyzlota'
-NBP_API_EX_RATES_URL = 'http://api.nbp.pl/api/exchangerates/tables/A/'
-CINKCIARZ_SCRAPING_RATE_URL = 'https://cinkciarz.pl/wa/pe/transactional?subscriptionId=PLN&unit=50000'
+def check_is_currency(currency, nbp_ex):
+    if currency in nbp_ex:
+        return True
+    else:
+        return False
 
-# function compere Value and add to the list
 
-
-def compperre(cin_ex, nbp_ex, all_currency):
+def comparison(cin_ex, nbp_ex, all_currency):
     result = []
-    for key in all_currency:
-        if key in nbp_ex and key in cin_ex:
-            if (float(nbp_ex[key]) <= float(cin_ex[key])):
+    for currency in all_currency:
+        if check_is_currency(currency, cin_ex):
+            if check_is_currency(currency, nbp_ex):
+                if (float(nbp_ex[currency]) <= float(cin_ex[currency],)):
+                    result.append(
+                        f'Lepszy przelicznik  {currency}  jest na NBP : {nbp_ex[currency]}')
+                else:
+                    result.append(
+                        f'Lepszy przelicznik  {currency}  jest na Cinkciarzu : {cin_ex[currency]}')
+            else:
                 result.append(
-                    f'Lepszy przelicznik  {key}  jest na NBP : {nbp_ex[key]}')
-            elif (float(nbp_ex[key]) > float(cin_ex[key])):
-                result.append(
-                    f'Lepszy przelicznik  {key}  jest na Cinkciarzu : {cin_ex[key]}')
-        elif key in nbp_ex:
-            result.append(f'Waluta {key} tylko na NBP po  {nbp_ex[key]}')
-        elif key in cin_ex:
+                    f'Nie ma waluty na NBP przelicznik z Cinkciarza:  {cin_ex[currency]}')
+        else:
             result.append(
-                f'Waluta {key} tylko na Cinkciarzu po  {cin_ex[key]}')
+                f'Nie ma waluty na Cinkciarzu przelicznik z NBP:  {nbp_ex[currency]}')
     return result
 
-# function sum all currency
+
+def sum_currency(cin_ex, nbp_ex):
+    return list(ChainMap(cin_ex, nbp_ex))
 
 
-def sum_dict(cin_ex, nbp_ex):
-    my_list = []
-    for key in cin_ex:
-        my_list.append(key)
-    for key in nbp_ex:
-        my_list.append(key)
-    return list(set(my_list))
-
-
-gold_price = requests.get(NBP_API_GOLD_PRICE_URL).json().pop()
-ex_rates = requests.get(NBP_API_EX_RATES_URL).json().pop()
+ex_rates, *_ = requests.get(NBP_API_EX_RATES_URL).json()
 cin_ex_rates = requests.get(CINKCIARZ_SCRAPING_RATE_URL)
 
 
@@ -58,7 +55,6 @@ soup = BeautifulSoup(cin_ex_rates.text, "lxml")
 table = soup.find("table", attrs={"class": "table"})
 table_data = table.tbody.find_all("tr")
 
-# Get all the headings of Lists
 headings = []
 cin_ex_rate = {}
 for index in range(len(table_data)):
@@ -69,16 +65,16 @@ for index in range(len(table_data)):
     headings.append(currency)
 
 new_cin_ex_rates = {}
-for i in cin_ex_rate:
-    new_cin_ex_rates[(i.split().pop())] = cin_ex_rate[i].replace(',', '.')
+for rate in cin_ex_rate:
+    new_cin_ex_rates[(rate.split().pop())] = cin_ex_rate[rate].replace(',', '.')
 
 
-all_currency = sum_dict(new_cin_ex_rates, nbp_ex_rates)
+all_currency = sum_currency(new_cin_ex_rates, nbp_ex_rates)
 
 
 @app.route('/')
 def my_form():
-    return render_template('testy.html', wyniki=compperre(new_cin_ex_rates, nbp_ex_rates, all_currency))
+    return render_template('testy.html', wyniki=comparison(new_cin_ex_rates, nbp_ex_rates, all_currency))
 
 
 if __name__ == '__main__':
